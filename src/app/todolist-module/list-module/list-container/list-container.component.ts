34a9b46461998ef90a4bd25e50d4d7b43c22bdc8
InterLink Intern <intern@intern-pc.interlink.office>
2019-07-26 12:06:29 +0300

    create new branch, start implementation use rxjs

--- src/app/todolist-module/list-module/list-container/list-container.component.ts
+++ src/app/todolist-module/list-module/list-container/list-container.component.ts
@@ -3,7 +3,15 @@ import {List} from "../../../list";
 
 import {ListServiceService} from "../../../services/list-service.service";
 import {ActivatedRoute} from "@angular/router";
-import {error} from "util";
+import {BehaviorSubject, Observable, Subject, combineLatest} from "rxjs";
+import {map, scan, switchMap} from "rxjs/operators";
+
+//__add
+interface ListAction {
+  (lists: List[]): List[]
+}
+const applyAction = (lists, action) => action(lists);
+//__add
 
 @Component({
   selector: 'app-list-container',
@@ -20,7 +28,15 @@ export class ListContainerComponent implements OnInit {
   isListChosen;
   isFirstLoad = true;
 
-  listRouteId;
+  // listRouteId;
+
+  //__add
+  activeList$: Observable<List>;
+  lists$: Observable<List[]>;
+
+  actions$: Subject<ListAction> = new BehaviorSubject(lists => lists);
+  //__add
+
 
   @Output() getListData = new EventEmitter();
 
@@ -28,7 +44,7 @@ export class ListContainerComponent implements OnInit {
 
   ngOnInit() {
 
-    this.listRouteId = this.route.snapshot.paramMap.get('id');
+/*    this.listRouteId = this.route.snapshot.paramMap.get('id');
 
     this.listService.getLists()
       .subscribe((data: List[]) => {
@@ -39,11 +55,33 @@ export class ListContainerComponent implements OnInit {
         else {
           console.log('list not found');
         }
-      });
+      });*/
+
+    //__add
+    let listId$ =  this.route.paramMap.pipe(map(p => +p.get('id')));
+    let lists$ = this.listService.getLists();
+
+    this.activeList$ = combineLatest(listId$, lists$)
+      .pipe(map(([id, lists]) => lists.find(l => l.id == id)));
+
+    // this.activeList$.subscribe(this.select);
+
+    this.lists$ = lists$
+      .pipe(
+        switchMap((lists) => {
+          return this.actions$.pipe(
+            scan<ListAction, List[]>(applyAction, lists)
+          )
+        }),
+      );
+
+    this.lists$.subscribe(data => this.lists = data);
+    // __add
+
   }
 
   onSubmitList(inputName: any){
-    const list = {
+/*    const list = {
       listName: inputName
     };
 
@@ -51,7 +89,15 @@ export class ListContainerComponent implements OnInit {
       .subscribe((data: List) => {
         console.log('POST request is successful', data);
         this.lists.push(data);
-      }, error => console.error(error));
+      }, error => console.error(error));*/
+
+    //__add
+    this.listService.addList({ listName: inputName })
+      .pipe(
+        map(list => lists => [...lists, list])
+      )
+      .subscribe( action => {this.actions$.next(action)});
+    //__add
   }
 
   isActive(listId){
@@ -78,17 +124,28 @@ export class ListContainerComponent implements OnInit {
     });
   }
 
-  deleteList($event, listId){
+  deleteList(list){
     this.isListDelete = true;
 
-    this.lists.forEach((element, index) => {
+/*    this.lists.forEach((element, index) => {
       if (element.id === listId) {
         this.lists.splice(index, 1);
 
         this.listService.deleteList(element.id)
           .subscribe(() => console.log('DELETE is successful'), error => console.error(error));
       }
-    });
+    });*/
+
+    //__add
+    this.listService.deleteList(list.id)
+      .pipe(map(_ => lists => {
+        let index = lists.indexOf(list);
+        return lists.slice(0, index).concat(lists.slice(index + 1))
+      }))
+      .subscribe(
+        action => {this.actions$.next(action)}
+      );
+    //__add
 
     this.isListDelete = true;
     this.isListChosen = false;
